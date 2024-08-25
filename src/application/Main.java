32313b9713b499bc84f53af2daa961c596d26495
Fatima <fatimamannaa897@gmail.com>
2024-08-26 00:58:13 +0300

    fatima

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,23 +1,24 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage stage) throws Exception {		
+			
+		Parent loginfxml = FXMLLoader.load(getClass().getResource("/auth/login.fxml"));
+		
+		Scene loginScene= new Scene(loginfxml);
+		loginScene.getStylesheets().add(getClass().getResource("/application.css").getDefaultPort());
+		stage.setScene(loginScene);
+		stage.setTitle("login");
+		stage.show();
 	}
 	
 	public static void main(String[] args) {
